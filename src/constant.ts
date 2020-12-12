d1161d5631d41ee1328d72302df1083cffdc63a0
MamoruDS <mamoruds.io@gmail.com>
2020-12-13 02:58:03 +0900

    added: RULE_FN_MAP_RESERVED
    fixed: 'AND' misspelled as 'ADD'

--- src/constant.ts
+++ src/constant.ts
@@ -1,13 +1,15 @@
+import { RuleFnMap } from './types'
+
 type TYPE_SINGLE = 'FINAL'
-type TYPE_GROUP = 'ADD' | 'OR' | 'NOT'
+type TYPE_GROUP = 'AND' | 'OR' | 'NOT'
 
 const TYPE_NOT = 'NOT'
 const TYPE_SINGLE = ['FINAL']
-const TYPE_GROUP = ['ADD', 'OR', 'NOT']
+const TYPE_GROUP = ['AND', 'OR', 'NOT']
 const LEN_LIMIT: {
     [key in TYPE_GROUP]: number
 } = {
-    ADD: Infinity,
+    AND: Infinity,
     OR: Infinity,
     NOT: 1,
 }
@@ -15,4 +17,37 @@ const LEN_LIMIT: {
 const TYPE_RESERVED = [...TYPE_SINGLE, ...TYPE_GROUP]
 type TYPE_RESERVED = TYPE_SINGLE | TYPE_GROUP
 
-export { TYPE_NOT, TYPE_SINGLE, TYPE_GROUP, LEN_LIMIT, TYPE_RESERVED }
+const RULE_FN_MAP_RESERVED: RuleFnMap<TYPE_RESERVED> = {
+    AND: {
+        fn: (input: { lhs: number; rhs: number }) => {
+            if (typeof input['lhs'] == 'undefined') {
+                return Boolean(input['rhs'])
+            } else {
+                return Boolean(input['lhs'] & input['rhs'])
+            }
+        },
+    },
+    OR: {
+        fn: (input: { lhs: number; rhs: number }) => {
+            if (typeof input['lhs'] == 'undefined') {
+                return Boolean(input['rhs'])
+            } else {
+                return Boolean(input['lhs'] | input['rhs'])
+            }
+        },
+    },
+    NOT: {
+        fn: (input: { lhs: number; rhs: number }) => {
+            return !Boolean(input['rhs'])
+        },
+    },
+    FINAL: {
+        fn: () => {
+            return true
+        },
+    },
+}
+
+export { TYPE_NOT, TYPE_SINGLE, TYPE_GROUP, TYPE_RESERVED }
+
+export { LEN_LIMIT, RULE_FN_MAP_RESERVED }
