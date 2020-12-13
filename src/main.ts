44d790cbb11cd5b6632a12698f36ceda862199c7
MamoruDS <mamoruds.io@gmail.com>
2020-12-13 11:37:19 +0900

    modified: added new parameter showPlot to method RuleEngine.exec
    modified: replacing all console.* with println
    
    improved: RuleEngine now can show plot in terminal by using RuleLine

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import * as CONST from './constant'
-import { Rule, RuleFn, RuleFnMap, Path, Matches, RuleCheckError } from './types'
+import {
+    Rule,
+    RuleFn,
+    RuleFnMap,
+    Path,
+    Matches,
+    RuleLineStatus,
+    RuleCheckError,
+} from './types'
+import { RuleLine } from './line'
+import { println } from './utils'
+
+const config = {
+    silentMode: false,
+}
 
 class RuleEngine<A extends string, T extends string> {
     private _ruleFnMap: RuleFnMap<T>
@@ -49,18 +63,18 @@ class RuleEngine<A extends string, T extends string> {
         })
         if (!isSubRule() && typeof rule.action == 'undefined') {
             // TODO:
-            e(`[ERR] Missing property 'action' of rule`)
+            e(`Missing property 'action' of rule`)
         }
         if (CONST.TYPE_SINGLE.indexOf(rule.type) != -1) {
         } else if (CONST.TYPE_GROUP.indexOf(rule.type) != -1) {
             if (typeof rule.list == 'undefined') {
-                e(`[ERR] Missing property 'list' of rule group`)
+                e(`Missing property 'list' of rule group`)
             }
             if (
                 rule.list.length >
                 CONST.LEN_LIMIT[rule.type as CONST.TYPE_GROUP]
             ) {
-                e(`[ERR] Number of rules in group exceeds the limit`)
+                e(`Number of rules in group exceeds the limit`)
             }
             for (const r of rule.list) {
                 const res = this._check(r, path)
@@ -77,13 +91,13 @@ class RuleEngine<A extends string, T extends string> {
                     : !Array.isArray(rule.value))
             ) {
                 e(
-                    `[ERR] Invalid rule value: <${valType}> is required but given '${
+                    `Invalid rule value: <${valType}> is required but given '${
                         rule.value
                     }' as <${typeof rule.value}>`
                 )
             }
         } else {
-            e(`[ERR] Unknown type '${rule.type}'`)
+            e(`Unknown type '${rule.type}'`)
         }
         path.shift()
         return res
@@ -101,7 +115,7 @@ class RuleEngine<A extends string, T extends string> {
                 if (log) {
                     errs.push(e.message)
                 } else {
-                    console.error(e.message)
+                    println(e.message, 'error')
                 }
             } else {
                 throw e
@@ -114,10 +128,21 @@ class RuleEngine<A extends string, T extends string> {
         rule: Rule<A, T>,
         input: any,
         index: string[],
-        matches: object,
-        actions: A[]
+        matches: Matches<A>,
+        actions: A[],
+        show?: RuleLine<A, T>[]
     ) {
         let match: number
+        let line: RuleLine<A, T>
+        if (show) {
+            line = new RuleLine<A, T>(
+                rule.type,
+                rule.action,
+                index.length - 1,
+                rule.value
+            )
+            show.push(line)
+        }
         if (CONST.TYPE_RESERVED.indexOf(rule.type) != -1) {
             const update = (m: number) => {
                 match = CONST.RULE_FN_MAP_RESERVED[
@@ -136,7 +161,8 @@ class RuleEngine<A extends string, T extends string> {
                         input,
                         [...index, i],
                         matches,
-                        actions
+                        actions,
+                        show
                     )
                     update(m)
                 }
@@ -185,10 +211,41 @@ class RuleEngine<A extends string, T extends string> {
                 }
             }
         }
+        if (show) {
+            const _index = [...index]
+            let t = matches
+            let m: RuleLineStatus = match ? 1 : 0
+            while (_index.length != 0) {
+                const i = parseInt(_index.shift())
+                if (typeof t[i] == 'undefined') {
+                    line.update(m)
+                    break
+                }
+                t = t[i]
+                if (_index.length == 0) {
+                    const sub = Object.keys(t).filter((key) => {
+                        return key != 'match' && key != 'action'
+                    })
+                    if (sub.length == 0) {
+                        line.update(m)
+                        break
+                    } else {
+                        let matched = false
+                        for (const key of sub) {
+                            matched = matched || t[key]['match']
+                        }
+                        if (matched) {
+                            line.update(m == 0 ? -1 : 2)
+                        }
+                    }
+                }
+            }
+        }
         return match
     }
     exec(
         input: any,
+        showPlot: boolean = false,
         lazyMatch: boolean = true
     ): {
         matches: Matches<A>
@@ -203,11 +260,27 @@ class RuleEngine<A extends string, T extends string> {
         ]
         const matches = {} as Matches<A>
         const actions: A[] = []
+        const lines: RuleLine<A, T>[] = showPlot ? [] : undefined
+        if (showPlot && lazyMatch) {
+            println(
+                `lazyMatch will be disabled since showPlot has been enabled.`,
+                'info'
+            )
+        }
         for (const i in rules) {
-            this._test(rules[i], input, [i], matches, actions)
-            if (lazyMatch && actions.length > 0) {
+            this._test(rules[i], input, [i], matches, actions, lines)
+            if (!showPlot && lazyMatch && actions.length > 0) {
                 break
             }
+        }
+        if (showPlot) {
+            println(
+                lines
+                    .map((line) => {
+                        return line.toString()
+                    })
+                    .join('\n')
+            )
         }
         return {
             matches,
@@ -216,5 +289,6 @@ class RuleEngine<A extends string, T extends string> {
     }
 }
 
+export { config }
 export { Rule, RuleFn, RuleFnMap }
 export { RuleEngine, RuleCheckError }
